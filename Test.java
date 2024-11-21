7e364f51e8d0a78623bef1876dd39d47dfe3a911
Edara Avinash <avinashedara199@gmail.com>
2024-11-21 22:14:15 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,44 +1,5 @@
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter first number:");
-        double num1 = 10;
-
-        System.out.println("Enter an operator (+, -, *, /):");
-        char operator = '+';
-
-        System.out.println("Enter second number:");
-        double num2 = 5;
-
-        double result;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error: Division by zero");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Invalid operator!");
-                return;
-        }
-
-        System.out.println("The result is: " + result);
-        scanner.close();
+        System.out.println(10+5);
     }
 }
